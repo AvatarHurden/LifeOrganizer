199eaccf08bf82a3e919869c0352318d1a04df2b
Arthur Giesel Vedana <agieselvedana@gmail.com>
2015-03-16 20:29:31 -0300

    Finished basic funcionality of Task class

--- src/objects/Task.java
+++ src/objects/Task.java
@@ -1,5 +1,6 @@
 package objects;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.joda.time.DateTime;
@@ -28,21 +29,20 @@ public class Task {
 		
 		setCreationDate(new DateTime());
 		setEditDate(getCreationDate());
-		
 	}
 	
-	public State getState() {
-		return state;
-	}
-
 	public void setState(State state) {
 		this.state = state;
-		setEditDate(new DateTime());
+		setEditDate();
 		
 		if (state != State.TODO)
 			setCompletionDate(getEditDate());
 		else
 			setCompletionDate(null);
+	}
+	
+	private void setEditDate() {
+		this.editDate = new DateTime();
 	}
 
 	public String getName() {
@@ -51,6 +51,7 @@ public class Task {
 
 	public void setName(String name) {
 		this.name = name;
+		setEditDate();
 	}
 	
 	public DateTime getEditDate() {
@@ -76,5 +77,65 @@ public class Task {
 	public void setCreationDate(DateTime creationDate) {
 		this.creationDate = creationDate;
 	}
+	
+	public State getState() {
+		return state;
+	}
 
+	public void addProject(String string) {
+		if (projects == null)
+			projects = new ArrayList<String>();
+		if (!string.startsWith("+"))
+			string = "+" + string;
+		projects.add(string);
+		setEditDate();
+	}
+	
+	public void removeProject(String string) {
+		if (!string.startsWith("+"))
+			string = "+" + string;
+		projects.remove(string);
+	}
+	
+	public List<String> getProjects() {
+		return projects;
+	}
+	
+	public void addContext(String string) {
+		if (contexts == null)
+			contexts = new ArrayList<String>();
+		if (!string.startsWith("@"))
+			string = "@" + string;
+		contexts.add(string);
+		setEditDate();
+	}
+
+	public void removeContext(String string) {
+		if (!string.startsWith("@"))
+			string = "@" + string;
+		contexts.remove(string);
+	}
+
+	
+	public List<String> getContexts() {
+		return contexts;
+	}
+	
+	public void setNote(String string) {
+		this.note = string;
+		setEditDate();
+	}
+	
+	public String getNote() {
+		return note;
+	}
+	
+	public void setDueDate(DateTime due) {
+		this.dueDate = due;
+		setEditDate();
+	}
+	
+	public DateTime getDueDate() {
+		return dueDate;
+	}
 }
--- test/objects/TaskTest.java
+++ test/objects/TaskTest.java
@@ -1,12 +1,19 @@
 package objects;
 
-import static org.junit.Assert.assertEquals;
-
 import org.joda.time.DateTime;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TaskTest {
 
+	@Test
+	public void testCreationDate() {
+		Task t = setUp();
+		
+		Assert.assertEquals(new DateTime().getMillis(), t.getCreationDate().getMillis(), 1000);
+		Assert.assertEquals(new DateTime().getMillis(), t.getEditDate().getMillis(), 1000);
+	}
+	
 	@Test
 	public void testSetName() {
 		Task t = setUp();
@@ -14,43 +21,95 @@ public class TaskTest {
 		
 		t.setName(name);
 		
-		assertEquals(name, t.getName());
-	}
-	
-	@Test
-	public void testCreationDate() {
-		Task t = setUp();
-		DateTime d = new DateTime();
-		
-		assertEquals(d.getMillis(), t.getCreationDate().getMillis(), 1000);
+		Assert.assertEquals(name, t.getName());
+		Assert.assertTrue(wasEdited(t));
 	}
 	
 	@Test
 	public void testDoTask() {
 		Task t = setUp();
-		DateTime d = new DateTime();
 		
 		t.setState(Task.State.DONE);
 		
-		assertEquals(t.getState(), Task.State.DONE);
-		assertEquals(d.getMillis(), t.getCompletionDate().getMillis(), 1000);
-		assertEquals(d.getMillis(), t.getEditDate().getMillis(), 1000);
+		Assert.assertEquals(t.getState(), Task.State.DONE);
+		Assert.assertEquals(new DateTime().getMillis(), t.getCompletionDate().getMillis(), 1000);
+		Assert.assertTrue(wasEdited(t));
 	}
 	
 	@Test
 	public void testFailTask() {
+		
 		Task t = setUp();
-		DateTime d = new DateTime();
 		
 		t.setState(Task.State.FAILED);
 		
-		assertEquals(t.getState(), Task.State.FAILED);
-		assertEquals(d.getMillis(), t.getCompletionDate().getMillis(), 1000);
-		assertEquals(d.getMillis(), t.getEditDate().getMillis(), 1000);
+		Assert.assertEquals(t.getState(), Task.State.FAILED);
+		Assert.assertEquals(new DateTime().getMillis(), t.getCompletionDate().getMillis(), 1000);
+		Assert.assertTrue(wasEdited(t));	
 	}	
+	
+	@Test
+	public void testProjects() {
+		Task t = setUp();
+		
+		t.addProject("+UFRGS");
+		t.addProject("PC");
+		
+		Assert.assertArrayEquals(new String[] {"+UFRGS", "+PC"}, t.getProjects().toArray());
 
+		t.removeProject("+PC");
+		t.removeProject("UFRGS");
+		
+		Assert.assertArrayEquals(new String[0], t.getProjects().toArray());	
+		Assert.assertTrue(wasEdited(t));
+	}
+	
+	@Test
+	public void testContexts() {
+		Task t = setUp();
+		
+		t.addContext("@UFRGS");
+		t.addContext("Casa");
+		
+		Assert.assertArrayEquals(new String[] {"@UFRGS", "@Casa"}, t.getContexts().toArray());
+	
+		t.removeContext("@Casa");
+		t.removeContext("UFRGS");
+		
+		Assert.assertArrayEquals(new String[0], t.getContexts().toArray());
+		
+		
+		Assert.assertTrue(wasEdited(t));
+	}
+
+	@Test
+	public void testNotes() {
+		Task t = setUp();
+		
+		t.setNote("This is a note");
+		
+		Assert.assertEquals("This is a note", t.getNote());
+		Assert.assertTrue(wasEdited(t));
+	}
+	
+	@Test
+	public void testSetDue() {
+		Task t = setUp();
+		DateTime d = new DateTime().plusDays(2);
+		
+		t.setDueDate(d);
+		
+		Assert.assertEquals(d, t.getDueDate());
+		Assert.assertTrue(wasEdited(t));
+	}
+	
 	private Task setUp() {
 		return new Task();
+	}
+	
+	private boolean wasEdited(Task t) {
+		return Math.abs(new DateTime().getMillis() - t.getEditDate().getMillis()) < 1000
+				&& t.getCreationDate() != t.getEditDate();
 	}
 	
 }
