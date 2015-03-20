10ed148c367a6a1c31f29779223b2286b9b10c93
Arthur Giesel Vedana <agieselvedana@gmail.com>
2015-03-19 22:13:08 -0300

    Added basic funcionality to StoredList

--- src/tools/StoredList.java
+++ src/tools/StoredList.java
@@ -1,14 +1,71 @@
 package tools;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class StoredList<T> extends ArrayList<T> {
+public abstract class StoredList<T extends Comparable<T>> extends ArrayList<T> {
 
 	private File file;
+	private boolean sorted;
 	
 	public StoredList(File file, boolean sorted) {
 		this.file = file;
+		this.sorted = sorted;
+		try {
+			load();
+		} catch (IOException e) {}
 	}
+	
+	public void load() throws IOException {		
+		if (!file.exists())
+			file.createNewFile();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = reader.readLine()) != null)
+			add(decode(line));
+		reader.close();
+	}
+	
+	public void save() throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		for (T obj : this) {
+			String line = encode(obj);
+			writer.write(line);
+			if (!line.endsWith("\n"))
+				writer.newLine();
+		}
+		writer.close();
+	}
+	
+	public boolean add(T s) {
+		if (!sorted) {
+			super.add(s);
+			return true;
+		}
+		
+		int i = 0;
+		while (i < size() && get(i).compareTo(s) <= 0)
+			i++;
+		add(i, s);
+		return true;
+	}
+	
+	/**
+	 * Takes a single line of the file (without the line termination) and converts it into a instance of <class>T</class>
+	 * @param line
+	 */
+	abstract T decode(String line);
+	/**
+	 * Convert an instance of <class>T</class> into a line representation (without the line termination)
+	 * @param object
+	 */
+	abstract String encode(T object);
+	
 	
 }
--- test/tools/StoredListTest.java
+++ test/tools/StoredListTest.java
@@ -1,11 +1,15 @@
 package tools;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -30,8 +34,71 @@ public class StoredListTest {
 		assertEquals(0, list.size());
 	}
 	
+	@Test
+	public void ordersElements() {
+		StoredList<String> list = setUp();
+		
+		list.add("3last");
+		list.add("1first");
+		list.add("2second");
+		
+		assertArrayEquals(new String[] {"1first", "2second", "3last"}, list.toArray());
+	}
+	
+	@Test
+	public void testSave() {
+		StoredList<String> list = setUp();
+		
+		list.add("1first");
+		list.add("2second");
+		list.add("3last");
+		
+		try {
+			list.save();
+		
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			assertEquals("1first", reader.readLine());
+			assertEquals("2second", reader.readLine());
+			assertEquals("3last", reader.readLine());
+			assertEquals(null, reader.readLine());
+			reader.close();
+		} catch (IOException e) {
+			Assert.fail();
+		}
+		
+	}
+	
+	@Test
+	public void testSaveLoad() {
+		StoredList<String> list = setUp();
+		
+		list.add("1first");
+		list.add("2second");
+		list.add("3last");
+		
+		try {
+			list.save();
+		} catch (IOException e) {
+			Assert.fail();
+		}
+		StoredList<String> list2 = setUp();
+		
+		assertEquals(list, list2);
+	}
+	
 	private StoredList<String> setUp() {
-		return new StoredList<String>(file, true);
+		return new StoredList<String>(file, true) {
+			@Override
+			String decode(String line) {
+				return line;
+			}
+
+			@Override
+			String encode(String object) {
+				return object;
+			}
+
+		};
 	}
 	
 }
